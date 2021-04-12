33efbf57aa2da3d38b159ff76e709efc1e7fd173
Mykhailo Glubokyi <mee.six@gmail.com>
2021-04-12 18:29:40 +0300

    gatsby-node products cycle fix

--- gatsby-node.js
+++ gatsby-node.js
@@ -8,9 +8,9 @@ const {
 
 const pages = [
   {id:'home', slug:'/', component : path.resolve(`./src/templates/index.js`), data: require(`./src/content/home/home.json`)},
-  {id:'contact', slug:'contact', component : path.resolve(`./src/templates/contact.js`), data: require(`./src/content/contacts/contacts.json`)},
-  {id:'certification', slug:'certification', component : path.resolve(`./src/templates/certification.js`), data: require(`./src/content/certification/certification.json`)},
-  {id:'team', slug:'our_team', component : path.resolve(`./src/templates/team.js`), data: require(`./src/content/contacts/team.json`)},
+  {id:'contact', slug:'/contact', component : path.resolve(`./src/templates/contact.js`), data: require(`./src/content/contacts/contacts.json`)},
+  {id:'certification', slug:'/certification', component : path.resolve(`./src/templates/certification.js`), data: require(`./src/content/certification/certification.json`)},
+  {id:'team', slug:'/our_team', component : path.resolve(`./src/templates/team.js`), data: require(`./src/content/contacts/team.json`)},
 ];
 
 const contentPage = path.resolve(`./src/templates/page.js`);
@@ -38,7 +38,7 @@ exports.createPages = async ({ actions }) => {
 
   contentLocalized[language].forEach(page => {
     createPage({
-      path: localizeUrl(language, defaultLanguage, `/${page.slug}`),
+      path: localizeUrl(language, defaultLanguage, `/${page.id}`),
       component: contentPage,
       context: {
         page,
@@ -55,10 +55,11 @@ exports.createPages = async ({ actions }) => {
         products: productsLocalized[language],
       },
     });
-    
-    productsLocalized[language].forEach(page => {
+
+    productsLocalized[language][0].metadata.products_shop.forEach(page => {
+
       createPage({
-        path: localizeUrl(language, defaultLanguage, `/products/${page.slug}`),
+        path: localizeUrl(language, defaultLanguage, `/products/${page.id}`),
         component: singleProductPage,
         context: {
           product: page,
