ef258e1cbc9905d907eefff58117836b9d03ebc1
Mykhailo Glubokyi <mee.six@gmail.com>
2021-04-12 09:03:25 +0300

    Submission form send test

--- netlify/functions/submit-form/submit-form.js
+++ netlify/functions/submit-form/submit-form.js
@@ -1,17 +1,34 @@
-// Docs on event and context https://www.netlify.com/docs/functions/#the-handler-method
-const handler = async (event) => {
+const sgMail = require('@sendgrid/mail')
+const fs = require('fs')
+const { GATSBY_SENDGRID_API_KEY } = process.env
+    
+    
+exports.handler = async (event, context, callback) => {
+  const {body} = JSON.parse(event)
+  
+  //define pdf email content and sender
+  const msg = { 
+    to: 'mee.six@gmail.com',
+    from: 'info@proorganica.com',
+    subject: 'New website form submission',
+    html: `
+    <p>Email body</p>
+    
+    `,
+  }
+  
   try {
-    const subject = event.queryStringParameters.name || 'World'
+    sgMail.setApiKey(GATSBY_SENDGRID_API_KEY)
+    await sgMail.send(msg)
+
     return {
       statusCode: 200,
-      body: JSON.stringify({ message: `Hello ${subject}` }),
-      // // more keys you can return:
-      // headers: { "headerName": "headerValue", ... },
-      // isBase64Encoded: true,
+      body: 'Message sent',
     }
-  } catch (error) {
-    return { statusCode: 500, body: error.toString() }
+  } catch (e) {
+    return {
+      statusCode: e.code,
+      body: e.message,
+    }
   }
 }
-
-module.exports = { handler }
