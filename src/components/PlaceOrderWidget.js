64bcbfd5392280392c4a9dc6ed9d88bb7d782c20
Mykhailo Glubokyi <mee.six@gmail.com>
2021-04-13 18:24:42 +0300

    cleanup

--- src/components/PlaceOrderWidget.js
+++ src/components/PlaceOrderWidget.js
@@ -7,7 +7,7 @@ const PlaceOrderWidget = () => {
       <div class="PlaceOrderWidget">
       <hr/>
       <p>Contact us to learn more about our products <br/> and services or to place an order</p>
-      <a href={buildLink('contact#form')} className="PlaceOrder max-width"><h1>Place an Order</h1></a>xxx
+      <a href={buildLink('contact#form')} className="PlaceOrder max-width"><h1>Place an Order</h1></a>
       </div>
     );
 };
